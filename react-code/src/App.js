2256248e9162ef3dbf388529f783f49f258492d3
Daekyo-Jeong <dk_w_rld@naver.com>
2021-09-14 16:57:33 +0900

    20210914 | react navbar/button
    UI 클릭시 blooean 값 판단으로 클래스 명 변경하는 방식의 토글 스위치 구현
    
    버튼 ui 정의 및 컴포넌트로 활용
    => 차후 이미지 박스 등에 활용 가능성

--- react-code/src/App.js
+++ react-code/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Modal from './components/Modal';
 import RouteTest from './components/RouteTest';
 import Contents from './components/Contents';
+import Navbar from './components/Navbar';
 
 import {
   BrowserRouter as Router,
@@ -16,17 +17,24 @@ import {
 function App() {
 
   return (
-    <div className="App">
-      <nav className="nav">
+    <>
+    <Router>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/" exact></Route>
+      </Switch>
+    </Router>
+      {/* <nav className="nav">
         <h1>React test page</h1>
-      </nav>
-      <RouteTest></RouteTest>
-      <input type="button" value="button"/>
-      <Contents/>
+      </nav> */}
+      
+      {/* <RouteTest></RouteTest>
+      <input type="button" value="button"/> */}
+      {/* <Contents/> */}
       {/* 컴포넌트는 대분자로 시작해야한다. */}
-      <Modal/>
+      {/* <Modal/> */}
       {/* <Modal></Modal> */}
-    </div>
+    </>
   );
 }
 
--- react-code/src/components/Navbar.js
+++ react-code/src/components/Navbar.js
@@ -1,15 +1,78 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Navbar.css';
+import Button from './Button'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink,
+    useParams
+  } from "react-router-dom";
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    // const handleClick = () => setClick(!click);
+    const handleClick = function(){
+        setClick(!click);
+    }
+
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    },);
+
+    window.addEventListener('resize', showButton);
+    
+
     return(
         <>
-        <nav className = "navbar">
-            <div className = "navbar-container">
-
-            </div>
-        </nav>
+            <nav className="navbar">
+                <div className="navbar-contatiner">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        DKsladkfjlask
+                    </Link>
+                    {/* 클릭시 클래스명을 변경하여 CSS속성을 변경하는 토글 스위치 */}
+                    <div className="menu-icon" onClick={handleClick}>
+                        <p className={click ? 'openmenu' : 'closedmenu'}>menu</p>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                                Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                                Products
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle="btn--outline">Sign up</Button>}
+                </div>        
+            </nav>
         </>
     )
 }
